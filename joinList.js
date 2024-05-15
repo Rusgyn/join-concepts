bc1be22aac611a3e5767a4abaa441498450db0ec
rusgyn <rusgynmagsanay@gmail.com>
2024-05-15 15:41:51 -0400

    Implement a function that join the elements of an array.

--- joinList.js
+++ joinList.js
@@ -3,16 +3,39 @@
  * into one String, concepts, with each list item separated from
  * the previous by a comma.
  *
- * To implement this we'll create a joinList function which will take 
+ * To implement this we'll create a joinList function which will take
  * in any array of strings return a comma-separated string.
  *
  * Note: We can NOT use the built-in Array join function.
  */
 
 // Write our function (we must define it too!) below
-// ...
+// function that joins the arguments of a given array of strings
+// Array of strings.
+// the joined elements must be comma-separated.
+// do not use built-in .join method
+
+const joinList = function(arr) {
+  
+  let joinedList = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      joinedList = joinedList + arr[i];
+    } else if (arr[i] === "" || arr[i] === " ") {
+      joinedList;
+    } else {
+      joinedList = joinedList + arr[i] + ", ";
+    }
+  }
+  return joinedList;
+};
 
 // Test / Driver Code below...
 const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
 const concepts = joinList(conceptList);
 console.log(`Today I learned about ${concepts}.`);
+
+const list = ["apple", "", "carrot", " ", "eggplant"];
+const newList = joinList(list);
+console.log(`Today I learned about ${newList}.`);
